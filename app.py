9cbc42cd8259449151b77dbbcfafb41110aad84d
a-rahman092 <72246399+a-rahman092@users.noreply.github.com>
2021-09-04 11:00:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
 from flask import Flask, render_template, request, flash, redirect
-from flask_mail import Mail, Message
-import smtplib
+
  
 app = Flask(__name__)
 
 app.secret_key = '12345'
-
-mail= Mail(app)
-
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'abdulrahman209875@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Rahman@123'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
 
 @app.route('/', methods =['GET', 'POST'])
 
